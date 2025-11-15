d1f9d368d8d8e5fd421db0e615812f39e395cb83
athinaslime <athinaslime@gmail.com>
2025-11-16 01:03:12 +0200

    i18n: add greek language

--- src/i18n/Engine.cpp
+++ src/i18n/Engine.cpp
@@ -57,6 +57,9 @@ void I18n::initEngine() {
 
     // zh_Hant (Traditional Chinese)
     engine.registerEntry("zh_Hant", TXT_KEY_SEARCH_SOMETHING, "搜尋...");
+
+    // el_GR (Greek)
+    engine.registerEntry("el_GR", TXT_KEY_SEARCH_SOMETHING, "Αναζήτηστε...");
 }
 
 std::string I18n::localize(eTextKeys key, const Hyprutils::I18n::translationVarMap& vars) {
