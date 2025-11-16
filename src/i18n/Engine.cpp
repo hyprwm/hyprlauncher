dd88ed0f2b2cc8809095344933cc13c731943eab
Yamura <96625893+Yxmura@users.noreply.github.com>
2025-11-16 11:01:56 +0100

    Register multiple language entries in I18n engine
    added ko_kr and nl_be

--- src/i18n/Engine.cpp
+++ src/i18n/Engine.cpp
@@ -1,98 +1,86 @@
-#include "Engine.hpp"
-#include "../config/ConfigManager.hpp"
+// ar (Arabic)
+engine.registerEntry("ar", TXT_KEY_SEARCH_SOMETHING, "بحث...");
 
-static Hyprutils::I18n::CI18nEngine engine;
+// cs_CZ (Czech)
+engine.registerEntry("cs_CZ", TXT_KEY_SEARCH_SOMETHING, "Hledat...");
 
-//
-void I18n::initEngine() {
-    engine.setFallbackLocale("en_US");
+// da_DK (Danish)
+engine.registerEntry("da_DK", TXT_KEY_SEARCH_SOMETHING, "Søg...");
 
-    // ar (Arabic)
-    engine.registerEntry("ar", TXT_KEY_SEARCH_SOMETHING, "بحث...");
-    
-    // cs_CZ (Czech)
-    engine.registerEntry("cs_CZ", TXT_KEY_SEARCH_SOMETHING, "Hledat...");
-    
-    // da_DK (Danish)
-    engine.registerEntry("da_DK", TXT_KEY_SEARCH_SOMETHING, "Søg...");
-    
-    // de_DE (German)
-    engine.registerEntry("de_DE", TXT_KEY_SEARCH_SOMETHING, "Suche...");
-    
-    // el_GR (Greek)
-    engine.registerEntry("el_GR", TXT_KEY_SEARCH_SOMETHING, "Αναζήτηστε...");
-    
-    // en_US (English)
-    engine.registerEntry("en_US", TXT_KEY_SEARCH_SOMETHING, "Search...");
+// de_DE (German)
+engine.registerEntry("de_DE", TXT_KEY_SEARCH_SOMETHING, "Suche...");
 
-    // es (Spanish)
-    engine.registerEntry("es", TXT_KEY_SEARCH_SOMETHING, "Buscar...");
+// el_GR (Greek)
+engine.registerEntry("el_GR", TXT_KEY_SEARCH_SOMETHING, "Αναζήτηστε...");
 
-    // fa (Farsi/Persian)
-    engine.registerEntry("fa", TXT_KEY_SEARCH_SOMETHING, "جستجو...");
+// en_US (English)
+engine.registerEntry("en_US", TXT_KEY_SEARCH_SOMETHING, "Search...");
 
-    // fi_FI (Finnish)
-    engine.registerEntry("fi_FI", TXT_KEY_SEARCH_SOMETHING, "Hae...");
+// es (Spanish)
+engine.registerEntry("es", TXT_KEY_SEARCH_SOMETHING, "Buscar...");
 
-    // fr_FR (French)
-    engine.registerEntry("fr_FR", TXT_KEY_SEARCH_SOMETHING, "Rechercher...");
+// fa (Farsi/Persian)
+engine.registerEntry("fa", TXT_KEY_SEARCH_SOMETHING, "जستجو...");
 
-    // he (Hebrew)
-    engine.registerEntry("he", TXT_KEY_SEARCH_SOMETHING, "חיפוש...");
+// fi_FI (Finnish)
+engine.registerEntry("fi_FI", TXT_KEY_SEARCH_SOMETHING, "Hae...");
 
-    // hi_IN (Hindi)
-    engine.registerEntry("hi_IN", TXT_KEY_SEARCH_SOMETHING, "खोजें...");
+// fr_FR (French)
+engine.registerEntry("fr_FR", TXT_KEY_SEARCH_SOMETHING, "Rechercher...");
 
-    // hu_HU (Hungarian)
-    engine.registerEntry("hu_HU", TXT_KEY_SEARCH_SOMETHING, "Keresés...");
-    
-    // it_IT (Italian)
-    engine.registerEntry("it_IT", TXT_KEY_SEARCH_SOMETHING, "Cerca...");
-    
-    // ja_JP (Japanese)
-    engine.registerEntry("ja_JP", TXT_KEY_SEARCH_SOMETHING, "検索...");
+// he (Hebrew)
+engine.registerEntry("he", TXT_KEY_SEARCH_SOMETHING, "חיפוש...");
 
-    // ml_IN (Malayalam)
-    engine.registerEntry("ml_IN", TXT_KEY_SEARCH_SOMETHING, "തിരയുക...");
+// hi_IN (Hindi)
+engine.registerEntry("hi_IN", TXT_KEY_SEARCH_SOMETHING, "खोजें...");
 
-    // nl_NL (Dutch Netherlands)
-    engine.registerEntry("nl_NL", TXT_KEY_SEARCH_SOMETHING, "Zoeken...");
+// hu_HU (Hungarian)
+engine.registerEntry("hu_HU", TXT_KEY_SEARCH_SOMETHING, "Keresés...");
 
-    // pl_PL (Polish)
-    engine.registerEntry("pl_PL", TXT_KEY_SEARCH_SOMETHING, "Szukaj...");
+// it_IT (Italian)
+engine.registerEntry("it_IT", TXT_KEY_SEARCH_SOMETHING, "Cerca...");
 
-    // pt_BR (Portuguese BR)
-    engine.registerEntry("pt_BR", TXT_KEY_SEARCH_SOMETHING, "Buscar...");
-    
-    // pt_PT (Portuguese Portugal)
-    engine.registerEntry("pt_PT", TXT_KEY_SEARCH_SOMETHING, "Procurar...");
+// ja_JP (Japanese)
+engine.registerEntry("ja_JP", TXT_KEY_SEARCH_SOMETHING, "検索...");
 
-    // ro_RO (Romanian)
-    engine.registerEntry("ro_RO", TXT_KEY_SEARCH_SOMETHING, "Caută...");
+// ko_KR (Korean)
+engine.registerEntry("ko_KR", TXT_KEY_SEARCH_SOMETHING, "검색...");
 
-    // ru_RU (Russian)
-    engine.registerEntry("ru_RU", TXT_KEY_SEARCH_SOMETHING, "Поиск...");
-  
-    // sv_SE (Swedish)
-    engine.registerEntry("sv_SE", TXT_KEY_SEARCH_SOMETHING, "Sök...");
-    
-    // ta_IN (Tamil)
-    engine.registerEntry("ta_IN", TXT_KEY_SEARCH_SOMETHING, "தேடவும்...");
+// ml_IN (Malayalam)
+engine.registerEntry("ml_IN", TXT_KEY_SEARCH_SOMETHING, "തിരയുക...");
 
-    // vi_VN (Vietnamese)
-    engine.registerEntry("vi_VN", TXT_KEY_SEARCH_SOMETHING, "Tìm kiếm...");
+// nl_NL (Dutch Netherlands)
+engine.registerEntry("nl_NL", TXT_KEY_SEARCH_SOMETHING, "Zoeken...");
 
-    // zh (Simplified Chinese)
-    engine.registerEntry("zh", TXT_KEY_SEARCH_SOMETHING, "搜索...");
+// nl_BE (Dutch Belgium)
+engine.registerEntry("nl_BE", TXT_KEY_SEARCH_SOMETHING, "Zoeken...");
 
-    // zh_Hant (Traditional Chinese)
-    engine.registerEntry("zh_Hant", TXT_KEY_SEARCH_SOMETHING, "搜尋...");
-}
+// pl_PL (Polish)
+engine.registerEntry("pl_PL", TXT_KEY_SEARCH_SOMETHING, "Szukaj...");
 
-std::string I18n::localize(eTextKeys key, const Hyprutils::I18n::translationVarMap& vars) {
-    static auto POVERRIDELOCALE = Hyprlang::CSimpleConfigValue<Hyprlang::STRING>(g_configManager->m_config.get(), "locale:override");
+// pt_BR (Portuguese BR)
+engine.registerEntry("pt_BR", TXT_KEY_SEARCH_SOMETHING, "Buscar...");
 
-    const auto  LOCALE = std::string_view{*POVERRIDELOCALE}.empty() ? engine.getSystemLocale().locale() : std::string{*POVERRIDELOCALE};
+// pt_PT (Portuguese Portugal)
+engine.registerEntry("pt_PT", TXT_KEY_SEARCH_SOMETHING, "Procurar...");
 
-    return engine.localizeEntry(LOCALE, key, vars);
-}
+// ro_RO (Romanian)
+engine.registerEntry("ro_RO", TXT_KEY_SEARCH_SOMETHING, "Caută...");
+
+// ru_RU (Russian)
+engine.registerEntry("ru_RU", TXT_KEY_SEARCH_SOMETHING, "Поиск...");
+
+// sv_SE (Swedish)
+engine.registerEntry("sv_SE", TXT_KEY_SEARCH_SOMETHING, "Sök...");
+
+// ta_IN (Tamil)
+engine.registerEntry("ta_IN", TXT_KEY_SEARCH_SOMETHING, "தேடவும்...");
+
+// vi_VN (Vietnamese)
+engine.registerEntry("vi_VN", TXT_KEY_SEARCH_SOMETHING, "Tìm kiếm...");
+
+// zh (Simplified Chinese)
+engine.registerEntry("zh", TXT_KEY_SEARCH_SOMETHING, "搜索...");
+
+// zh_Hant (Traditional Chinese)
+engine.registerEntry("zh_Hant", TXT_KEY_SEARCH_SOMETHING, "搜尋...");
