6c294b586c2e12b654bbfec1c2684898ff32b406
Vaxry <vaxry@vaxry.net>
2026-08-04 13:29:13 +0200

    finders/desktop: use keywords for fuzzables as well
    fixes #150

--- src/finders/Fuzzy.hpp
+++ src/finders/Fuzzy.hpp
@@ -9,5 +9,5 @@
 
 namespace Fuzzy {
     std::vector<SP<IFinderResult>> getNResults(const std::vector<SP<IFinderResult>>& in, const std::string& query, size_t results);
-    std::vector<std::string>       createFuzzableStrings(std::initializer_list<std::string_view>, bool toLowercase = true);
+    std::vector<std::string>       createFuzzableStrings(std::vector<std::string_view>&&, bool toLowercase = true);
 };
